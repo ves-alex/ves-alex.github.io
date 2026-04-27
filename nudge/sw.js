eafcfd6dcde5d3dea0a8122c5bd55e00b7cef376
X37A <alexves.tech@pm.me>
2026-04-27 23:45:37 +0200

    sw: switch to network-first strategy (v12)

--- nudge/sw.js
+++ nudge/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nudge-v11";
+const CACHE_NAME = "nudge-v12";
 const ASSETS = [
   "./",
   "./index.html",
@@ -33,15 +33,14 @@ self.addEventListener("fetch", (event) => {
   if (url.origin !== self.location.origin) return;
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => caches.match("./index.html"));
-    })
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then((cached) => cached || caches.match("./index.html"))
+      )
   );
 });
